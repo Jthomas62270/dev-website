7ff0e2e86fa1f2c02e05eb5b581743578b3366c6
Tubby-john <91159100+Tubby-john@users.noreply.github.com>
2024-02-01 16:08:14 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import NavBar from './Componets/Public/NavBar';
+import Body from './Componets/Main/Body';
+import {BrowserRouter, Routes,  Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500&family=Inter&display=swap');
+      </style>
+      <NavBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}/>
+          </Routes>
+        </BrowserRouter>
     </div>
+
   );
 }
 
